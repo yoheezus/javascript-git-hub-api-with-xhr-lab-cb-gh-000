2dabe79281cd60ded7860bc6f5b4240e3a60afd8
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 17:39:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function displayRepositories() {
 }
 
 function getCommits(el) {
-    console.log(username)
+    console.log(username.value)
     const name = el.dataset.repo
     const req = new XMLHttpRequest()
     req.addEventListener("load", showCommits)
