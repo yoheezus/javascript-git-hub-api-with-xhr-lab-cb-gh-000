2a9ed067ab0f22dc81ad9e0963f8d1aa325457a2
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 23:37:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function displayRepositories() {
         return (`
                 <li>
                     <h2>${repo.name}</h2>
-                    <a href="${repo.html_url}>${repo.html_url}</a><br>
+                    <a href="${repo.html_url}">${repo.html_url}</a><br>
                     <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-                    <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+                    <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a>
                 </li>`
             )
     }).join("") + "</ul>"
