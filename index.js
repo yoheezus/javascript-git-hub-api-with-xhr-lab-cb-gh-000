90fa6469bfe97934fef580b2905754e80ac10d76
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 23:42:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ function displayRepositories() {
         const dataUsername = 'data-username="' + repo.owner.login + '"'
         const dataRepoName = 'data-repository="' + repo.name + '"'
         return (`
-            <li>
-                <h2>${repo.name}</h2>
-                <a href="${repo.html_url}">${repo.html_url}</a><br>
-                <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-                <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-            </li>`
+                <li>
+                    <h2>${repo.name}</h2>
+                    <a href="${repo.html_url}">${repo.html_url}</a><br>
+                    <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+                    <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+                </li>`
             )
+
     }).join("") + "</ul>"
     // const repoList = `<ul>${repos.map(
     //     r =>
