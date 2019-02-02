c65df5808d5532823d699e49ffdcd491a6e1c891
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 23:29:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 function getRepositories() {
     var username = document.getElementById("username").value;
-    console.log(username)
     const  req = new XMLHttpRequest();
     req.addEventListener("load", displayRepositories)
     req.open("GET", "https://api.github.com/users/" + username + "/repos")
